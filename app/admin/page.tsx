c9f18bb3db19a7872b690eeb08548c047ead7494
tomtarpeyyt <ttarpey58@gmail.com>
2024-04-09 22:32:38 +0100

    created an initial dashboard layout

---
+++ app/admin/page.tsx
@@ -0,0 +1,31 @@
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+
+export default function AdminDashboard() {
+    return (
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+            <DashboardCard title="Sales" subtitle="description..." body="some body text in here" />
+            <DashboardCard title="Products" subtitle="description..." body="some body text in here" />
+            <DashboardCard title="Customers" subtitle="description..." body="some body text in here" />
+        </div>
+    );
+}
+
+type DashboardCardProps = {
+    title: string
+    subtitle: string
+    body: string
+}
+
+function DashboardCard({title, subtitle, body}: DashboardCardProps) {
+    return (
+        <Card>
+            <CardHeader>
+                <CardTitle>{title}</CardTitle>
+                <CardDescription>{subtitle}</CardDescription>
+            </CardHeader>
+            <CardContent>
+                <p>{body}</p>
+            </CardContent>
+        </Card>
+    )
+}
