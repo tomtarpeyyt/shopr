b87f75037f7ebc80e159203b8e7ea5fc2ac936c2
tomtarpeyyt <ttarpey58@gmail.com>
2024-04-10 17:27:13 +0100

    pulling sales data from DB

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,9 +1,21 @@
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import db from "@/db/db";
+import { formatCurrency, formatNumber } from "@/lib/formatters";
 
-export default function AdminDashboard() {
+async function getSalesData() {
+    const data = await db.order.aggregate({
+        _sum: { pricePaidInPennies: true },
+        _count: true
+    });
+
+    return { amount: (data._sum.pricePaidInPennies || 0) / 100, numberOfSales: data._count };
+}
+
+export default async function AdminDashboard() {
+    const salesData = await getSalesData();
     return (
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-            <DashboardCard title="Sales" subtitle="description..." body="some body text in here" />
+            <DashboardCard title="Sales" subtitle={ formatNumber(salesData.numberOfSales) } body={ formatCurrency(salesData.amount) } />
             <DashboardCard title="Products" subtitle="description..." body="some body text in here" />
             <DashboardCard title="Customers" subtitle="description..." body="some body text in here" />
         </div>
