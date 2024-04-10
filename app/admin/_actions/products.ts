14e92a09701acd7edf50667381ebaf63e5075705
tomtarpeyyt <ttarpey58@gmail.com>
2024-04-10 22:21:24 +0100

    added some validation for the form

--- app/admin/_actions/products.ts
+++ app/admin/_actions/products.ts
@@ -1,5 +1,46 @@
 'use server';
 
-export async function addProduct(formData: FormData) {
-    console.log('FORM DATA', formData);
+import db from "@/db/db";
+import { z } from "zod";
+import fs from 'fs/promises';
+import { redirect } from "next/navigation";
+
+const fileSchema = z.instanceof(File, { message: 'Required' });
+const imageSchema = fileSchema.refine(file => file.size === 0 || file.type.startsWith('image/'));
+
+const addProductSchema = z.object({
+    name: z.string().min(3),
+    description: z.string().min(3),
+    priceInPence: z.coerce.number().int().min(1),
+    file: fileSchema.refine(file => file.size > 0, 'Required'),
+    image: imageSchema.refine(file => file.size > 0, 'Required')
+})
+
+export async function addProduct(prevState: unknown, formData: FormData) {
+    const result = addProductSchema.safeParse(Object.fromEntries(formData.entries()));
+    if (result.success === false) {
+        return result.error.formErrors.fieldErrors;
+    }
+
+    const data = result.data;
+
+    await fs.mkdir('products', { recursive: true});
+    const filePath = `products/${crypto.randomUUID()}-${data.file.name}`;
+    await fs.writeFile(filePath, Buffer.from(await data.file.arrayBuffer()))
+
+    await fs.mkdir('public/products', { recursive: true});
+    const imagePath = `products/${crypto.randomUUID()}-${data.image.name}`;
+    await fs.writeFile(`public/${imagePath}`, Buffer.from(await data.image.arrayBuffer()))
+
+
+    await db.product.create({ data: {
+        name: data.name,
+        description: data.description,
+        priceInPennies: data.priceInPence,
+        filePath,
+        imagePath
+        },
+    });
+
+    redirect('/admin/products');
 }
