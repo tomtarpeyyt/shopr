6f4f77efe4ae245b2988c7ea4ec75bc3a08b46cc
tomtarpeyyt <ttarpey58@gmail.com>
2024-04-10 21:32:46 +0100

    initial add new product form

---
+++ app/admin/products/_components/product-form.tsx
@@ -0,0 +1,41 @@
+'use client';
+
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Textarea } from "@/components/ui/textarea";
+import { formatCurrency } from "@/lib/formatters";
+import { useState } from "react";
+
+export default function ProductForm() {
+    const [priceInPence, setPriceInPence] = useState<number>();
+
+    return (
+        <form className='space-y-8'>
+            <div className='space-y-2'>
+                <Label htmlFor='name'>Name</Label>
+                <Input type='text' id='name' name='name' required />
+            </div>
+            <div className='space-y-2'>
+                <Label htmlFor='price'>Price in Pence</Label>
+                <Input type='number' id='priceInPence' name='priceInPence' required value={priceInPence} onChange={e => setPriceInPence(Number(e.target.value) || undefined)}/>
+                <div className="text-muted-foreground">
+                    { formatCurrency((priceInPence || 0) / 100) }
+                </div>
+            </div>
+            <div className='space-y-2'>
+                <Label htmlFor='description'>Description</Label>
+                <Textarea id='description' name='description' required />
+            </div>
+            <div className='space-y-2'>
+                <Label htmlFor='file'>File</Label>
+                <Input type='file' id='file' name='file' required />
+            </div>
+            <div className='space-y-2'>
+                <Label htmlFor='image'>Image</Label>
+                <Input type='file' id='image' name='image' required />
+            </div>
+            <Button type='submit'>Save</Button>
+        </form>
+    );
+}
---
+++ app/admin/products/new/page.tsx
@@ -0,0 +1,11 @@
+import AdminPageHeader from "../../_components/page-header";
+import ProductForm from "../_components/product-form";
+
+export default function NewProductPage() {
+    return (
+        <>
+            <AdminPageHeader>Add Product</AdminPageHeader>
+            <ProductForm />
+        </>
+    );
+}
