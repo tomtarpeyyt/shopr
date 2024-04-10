64b4599252962c64aa242d3464eee828e14de69f
tomtarpeyyt <ttarpey58@gmail.com>
2024-04-10 17:56:35 +0100

    getuser data and product data

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -11,13 +11,36 @@ async function getSalesData() {
     return { amount: (data._sum.pricePaidInPennies || 0) / 100, numberOfSales: data._count };
 }
 
+async function getUserData() {
+    const [ userCount, orderData ] = await Promise.all([
+        db.user.count(),
+        db.order.aggregate({ _sum: { pricePaidInPennies: true } })
+    ])
+
+    return {
+        userCount,
+        averageValuePerUser: userCount === 0 ? 0 : (orderData._sum.pricePaidInPennies || 0) / userCount / 100,
+    }
+    
+}
+
+async function getProductData() {
+    const [activeCount, inactiveCount ] = await  Promise.all([
+        db.product.count({where: { isAvailableForPurchase: true }}),
+        db.product.count({where: { isAvailableForPurchase: false }})
+    ]);
+
+    return { activeCount, inactiveCount };
+
+}
 export default async function AdminDashboard() {
-    const salesData = await getSalesData();
+    const [ salesData, userData, productData ]  = await Promise.all([ getSalesData(), getUserData(), getProductData() ]);
+
     return (
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-            <DashboardCard title="Sales" subtitle={ `${formatNumber(salesData.numberOfSales)} orders` } body={ formatCurrency(salesData.amount) } />
-            <DashboardCard title="Products" subtitle="description..." body="some body text in here" />
-            <DashboardCard title="Customers" subtitle="description..." body="some body text in here" />
+            <DashboardCard title="Sales" subtitle={ `${formatNumber(salesData.numberOfSales)} Orders` } body={ formatCurrency(salesData.amount) } />
+            <DashboardCard title="Customers" subtitle={ `${ formatCurrency(userData.averageValuePerUser) } Average Value` } body={ formatNumber(userData.userCount) } />
+            <DashboardCard title="Active Products" subtitle={`${ formatNumber(productData.inactiveCount) } Inactive`} body={ formatNumber(productData.activeCount) } />
         </div>
     );
 }
